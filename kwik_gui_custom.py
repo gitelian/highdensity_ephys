73bdf872032bbeb7c7f19dc685b88aff1a2e2272
gitelian <gitelian@berkeley.edu>
2016-10-24 16:22:39 -0700

    updates plots when new cluster is selected. plots ISI dist, amp vs t, and electrode drift

--- kwik_gui_custom.py
+++ kwik_gui_custom.py
@@ -21,62 +21,87 @@ class kwik_gui_custom(object):
                 gui = c.create_gui()
                 gui.show()
                 self.c = c
+                self.gui = gui
+                self.enable_plot = True
             except:
                 raise ValueError('Cannot open gui!--go find Greg!')
         else:
             raise ValueError('Kwik file not found!--make sure the path is correct')
 
+        @self.gui.connect_
+        def on_select(cluster_ids):
+            self.plot_metrics(cluster_id=cluster_ids[0])
+
     def plot_metrics(self, cluster_id=None, bin_size=0.0005, max_time=0.100):
 
-        # close any open plots
-        plt.close()
-        if cluster_id:
-            # get indices of specified cluster
-            uind = np.where(self.c.spike_clusters == cluster_id)[0]
+        if self.enable_plot is False:
+            # do nothing
+            None
         else:
-            print('no cluster ID was provided')
+            # close any open plots
+            plt.close()
+            if cluster_id:
+                # get indices of specified cluster
+                uind = np.where(self.c.spike_clusters == cluster_id)[0]
+            else:
+                print('no cluster ID was provided')
 
-        if len(uind) ==  0:
-            print('cluster ID not found')
-        else:
-            # get spike times and bin them
-            spk_times = self.c.spike_times[uind]
-            isi       = np.diff(spk_times)
-            bins      = np.arange(0, max_time, bin_size);
-            counts    = np.histogram(isi, bins=bins)[0]
-            rpvs      = np.where(isi < 0.0015)[0]
-            rpv_perc  = len(rpvs)/float(len(isi))*100
+            if len(uind) ==  0:
+                print('cluster ID not found')
+            else:
+                # get spike times and bin them
+                spk_times = self.c.spike_times[uind]
+                isi       = np.diff(spk_times)
+                bins      = np.arange(0, max_time, bin_size);
+                counts    = np.histogram(isi, bins=bins)[0]
+                rpvs      = np.where(isi < 0.0015)[0]
+                rpv_perc  = len(rpvs)/float(len(isi))*100
 
-            # get waveform snippets and measure amplitude TODO TEST ALL THIS!!!
-            samp_perc  = 0.1 # size of sample to draw from unit indices (so things actually plot quickly)
-            rand_inds  = np.random.choice(uind, int(uind*samp_perc)
-            waves      = self.c.all_waveforms[uind[rand_inds]]
-            wave_times = spk_times[rand_inds]
-            mean_waves = waves.mean(axis=0)
-            best_chan  = np.argmin(np.min(mean_waves, axis=0))
-            best_waves = waves[:, :, best_chan] # snippets x samples x channels
-            amp        = np.max(best_waves, axis=1) - np.min(best_waves, axis=1)
-            wave_drift = np.argmin(np.min(waves, axis=1), axis=1) # check this does is measure the drift of the largest min across channels?
-            # moving average method from stackoverflow (TODO TEST THIS)
-            cumsum     = np.cumsum(numpy.insert(amp, 0, 0))
-            N          = 10
-            avg_amp    = (cumsum[N:] - cumsum[:-N]) / N
+                # get waveform snippets and measure amplitude
+                samp_perc  = 0.01 # size of sample to draw from unit indices (so things actually plot quickly)
+                rand_inds  = np.sort(np.random.choice(uind, int(uind.shape[0]*samp_perc)))
+                waves      = self.c.all_waveforms[rand_inds] # 4 seconds
+                wave_times = self.c.spike_times[rand_inds] ### USE ORIGINAL SPIKE TIMES
+                mean_waves = waves.mean(axis=0) # average waveform for each channel
+                best_chan  = np.argmin(np.min(mean_waves, axis=0)) # samples x channels (find channel with largest waveform)
+                best_waves = waves[:, :, best_chan] # snippets x samples x channels
+                amp        = np.max(best_waves, axis=1) - np.min(best_waves, axis=1)
+                wave_drift = np.argmin(np.min(waves, axis=1), axis=1)
+                # moving average method from stackoverflow (http://stackoverflow.com/questions/13728392/moving-average-or-running-mean)
+                N          = 10
+                avg_amp    = np.convolve(amp, np.ones((N,))/N, mode='valid')
+                samp_diff  = wave_times.shape[0] - avg_amp.shape[0]
+                t_avg_amp  = wave_times[0:-samp_diff]
 
-            # plot isi distribution
-            rp_height = np.max(counts)*1.1
-            cluster_group = self.c.cluster_groups[58]
-            f, ax_hist = plt.subplots(1, 1)
-            plt.bar(bins[:-1], counts, width=bin_size, color='#4f8fff',\
-                    align='center', edgecolor='none')
-            ax_hist.add_patch(
-                    patches.Rectangle((0.0, 0.0), 0.0015, rp_height,
-                        facecolor='#ff0000', edgecolor='#ff0000', alpha=0.5))
-            plt.title('cluster ID: ' + str(cluster_id) + \
-                    'cluster group: ' + cluster_group + \
-                    'rbvs: ' + str(rpvs) + '/' + str(len(isi)) + '   ({:2.2f})%'.format(rpv_perc))
+                # prepare figure
+                rp_height = np.max(counts)*1.1
+                cluster_group = self.c.cluster_groups[cluster_id]
+                f, ax = plt.subplots(1, 2, figsize=(16, 6))
 
-            ## TODO add suplots with grid n stuff
+                # isi distribution
+                ax0 = plt.subplot(1, 2, 1)
+                plt.bar(bins[:-1], counts, width=bin_size, color='#4f8fff',\
+                        align='center', edgecolor='none')
+                ax0.add_patch(
+                        patches.Rectangle((0.0, 0.0), 0.0015, rp_height,
+                            facecolor='#ff0000', edgecolor='#ff0000', alpha=0.5))
+                plt.suptitle('cluster ID and group: ' + str(cluster_id) + ', ' + cluster_group + \
+                        '\nrbvs: ' + str(len(rpvs)) + '/' + str(len(isi)) + '   ({:2.2f})%'.format(rpv_perc))
+                plt.title('ISI distribution')
+                plt.xlabel('time (s)')
+                plt.ylabel('counts/bin')
 
-            plt.show()
+                # amplitude and drift vs time
+                ax1 = plt.subplot(1, 2, 2)
+                plt.plot(t_avg_amp, avg_amp, color='#4f8fff', linewidth=2)
+                plt.xlabel('time (s)')
+                plt.ylabel('peak-to-peak amplitude')
+                plt.title('amplitude and electrode drift')
+
+                ax2 = ax[1].twinx()
+                ax2.plot(wave_times, wave_drift, color='#ff0000', alpha=0.5)
+                ax2.set_ylabel('electrode contact')
+
+                plt.show()
 
 
