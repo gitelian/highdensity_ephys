39d6c43d8dcf7d6e6ceb908ff6cfa169935cbe14
gitelian <gitelian@berkeley.edu>
2017-11-01 13:50:18 -0700

    determines if a trial is a catch or opto stimulation trial. correctly updates the stimulus ID. adds lick times to lick_cell. needs debugging.

--- spkgad2run.m
+++ spkgad2run.m
@@ -75,6 +75,11 @@ encoder(find(diff(dio.channelData(stim_ind.running).data) > 0)+1) = 1;
 run_dist        = cumsum(encoder);
 run_cell        = cell(num_state_changes/2, 1);
 
+% make a lick and led_opto cell if licking data is available
+if jb_behavior == 1
+    lick_cell = cell(num_state_changes/2, 1);
+end
+
 trial_count     = 1;
 progressbar('extracting running distance')
 
@@ -86,24 +91,57 @@ for k = 1:2:num_state_changes - 1 % jumping by 2 will always select the start ti
         ind0 = state_change_inds(k) - dt_before_after(trial_count);   % start time index
         ind1 = state_change_inds(k+1) + dt_before_after(trial_count + 1); % stop time index
     end
-
+    
     % determine what stimulus was presented by counting the number of high
     % pusles on the second digital input line.
     if jb_behavior == 0
         num_pulses                     = length(find(diff(dio.channelData(stim_ind.stim_id).data(ind0:ind1)) < 0));
         stimsequence(trial_count)      = num_pulses;
         stimulus_times(trial_count, :) = (double([state_change_inds(k), state_change_inds(k+1)]) - double(ind0))/30000; % gets time of stimulus/trial start
+        
     elseif jb_behavior == 1
-        num_pulses                     = length(find(diff(dio.channelData(stim_ind.stim_id).data(last_trial_index:ind1)) < 0));
-        stimsequence(trial_count)      = num_pulses;
+        
+        % led_opto should be 0 if LED was off and 1 if LED was on
+        led_opto = length(find(diff(dio.channelData(stim_ind.LED_opto).data(last_trial_index:ind1) > 0)));
+        
+        % offset stim IDs by 9 to indicate LED/optogenetic trials vs no stim trials
+        if led_opto == 1
+            stim_offset = 9;
+        elseif led_opto == 0
+            stim_offset = 0;
+        end
+        
+        % determine which stimulus was presented or if it was a catch trial
+        num_pulses  = length(find(diff(dio.channelData(stim_ind.stim_id).data(last_trial_index:ind1)) < 0));
+        high_times  = find(diff(dio.channelData(stim_ind.stim_id).data(last_trial_index:ind1)) > 0);
+        low_times   = find(diff(dio.channelData(stim_ind.stim_id).data(last_trial_index:ind1)) < 0);
+        diff_times  = (low_times - high_times)/30000;
+        catch_trial = find(diff_times > 6);
+        
+        if catch_trial
+            stimsequence(trial_count) = 9 + stim_offset;
+        else
+            stimsequence(trial_count) = num_pulses + stim_offset;
+        end
+        
+        % get the lick indices for this trial subtract off the trial start
+        % time and convert to seconds
+        lick_times = (find(diff(dio.channelData(stim_ind.licking).data(ind0:ind1) < 0)) - state_change_inds(k))/30000;
+        % add lick times to lick cell
+        lick_cell{trial_count, 1} = lick_times;
+        
+        % save the trial start and stop times (dio channel 1/9)
         stimulus_times(trial_count, :) = (double([state_change_inds(k), state_change_inds(k+1)]) - double(ind0))/30000; % gets time of stimulus/trial start
-        last_trial_index               = ind1;
+        
+        % update last trial index
+        last_trial_index = ind1;
     end
 
     % add run distance to run cell
     run_cell{trial_count, 1} = run_dist(ind0:ind1);
     progressbar(trial_count/(num_state_changes/2));
     trial_count = trial_count + 1;
+    
 end
 
 progressbar(1)
